16802172b623f3486a77f6f44b668308328860af
Jo Van Bulck <jo.vanbulck@cs.kuleuven.be>
2020-07-13 20:05:40 +0200

    travis-ci: speed up sancus-bsl example

--- sancus/bsl/sm-bsl.h
+++ sancus/bsl/sm-bsl.h
@@ -11,9 +11,17 @@ char SM_ENTRY(sm_bsl) BSL430_unlock_BSL_balanced(char* data);
 #define SUCCESSFUL_OPERATION 0x00
 #define BSL_PASSWORD_ERROR 0x05
 
-#define INTERRUPT_VECTOR_START 0xFFE0
-#define INTERRUPT_VECTOR_END   0xFFFF
-#define BSL_PASSWORD_LENGTH    (INTERRUPT_VECTOR_END - INTERRUPT_VECTOR_START + 1)
+
+/* NOTE: speed up Travis-CI builds */
+#ifdef TRAVIS
+    #define BSL_PASSWORD_LENGTH    2
+    #define INTERRUPT_VECTOR_START (dummy_pwd)
+    extern char dummy_pwd[BSL_PASSWORD_LENGTH];
+#else
+    #define INTERRUPT_VECTOR_START 0xFFE0
+    #define INTERRUPT_VECTOR_END   0xFFFF
+    #define BSL_PASSWORD_LENGTH    (INTERRUPT_VECTOR_END - INTERRUPT_VECTOR_START + 1)
+#endif
 
 //#define LEAK_BSL_TSC
 #ifdef LEAK_BSL_TSC
